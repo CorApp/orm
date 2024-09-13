c15a1dea4b3359b912377eaea39ba14b534bbb53
Carmelo Campos <carmelogunsroses@gmail.com>
2024-09-13 14:48:58 -0500

    feat: base orm

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,8 @@
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const fooTable = sqliteTable("foo", {
-  bar: text("bar").notNull().default("Hey!"),
+export const users = sqliteTable("users", {
+  id: text("id").primaryKey().unique(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
 });
---
+++ test.ts
@@ -0,0 +1,13 @@
+import db from "@/db";
+import { users } from "@db/schema";
+
+// Insert
+await db.insert(users).values({
+  id: Date.now().toString(),
+  name: "John Doe",
+  email: Date.now().toString() + "test2@uwu.com",
+  password: "password",
+});
+
+const result = await db.select().from(users).all();
+console.log(result);
