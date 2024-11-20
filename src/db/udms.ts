ad65f654770d6d90dfa3bcc64f9b74b917acba05
Carmelo Campos <carmelogunsroses@gmail.com>
2024-11-20 18:44:35 -0500

    build: :bookmark: 1.14.0

--- src/db/udms.ts
+++ src/db/udms.ts
@@ -4,9 +4,9 @@ import { v4 } from "uuid";
 const udms = sqliteTable("udms", {
   id: text("id").primaryKey().unique().$default(v4),
   name: text("name").notNull(),
-  height: numeric("height").notNull(),
-  width: numeric("width").notNull(),
-  length: numeric("length").notNull(),
+  height: numeric("height").notNull().default("1"),
+  width: numeric("width").notNull().default("1"),
+  length: numeric("length").notNull().default("1"),
   weight: numeric("weight").notNull().default("1"),
 });
 
