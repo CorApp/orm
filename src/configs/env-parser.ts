ff6f26333fa127e4f9c14395bf962413eccf4e7e
ccasas19 <cristian_casas19@outlook.com>
2026-06-13 10:32:14 -0500

    Commit env.parser

--- src/configs/env-parser.ts
+++ src/configs/env-parser.ts
@@ -3,4 +3,6 @@ import { z } from "zod";
 export const envSchema = z.object({
   TURSO_DATABASE_URL: z.string().url(),
   TURSO_AUTH_TOKEN: z.string(),
+  TURSO_SAAS_URL: z.string().url(),
+  TURSO_SAAS_TOKEN: z.string(),
 });
